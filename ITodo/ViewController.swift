fea6bf57cefe543e762ffa376d06ef87d84eade4
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-03 19:42:12 +0900

    ・Todoアプリケーションのプロジェクトを作成する。

---
+++ ITodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITodo
+//
+//  Created by xda-yoshioka on 2019/10/03.
+//  Copyright © 2019 velo.yamigiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
