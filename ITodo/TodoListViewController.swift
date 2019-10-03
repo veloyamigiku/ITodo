51a38c3f1673cb7488aae4b71d218e6bed3b06da
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-03 19:57:54 +0900

    ・Todo一覧画面をレイアウトする。 ・Todoを一覧表示する。一覧表示ロジックは最小限の実装とする。

---
+++ ITodo/TodoListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TodoListViewController.swift
+//  ITodo
+//
+//  Created by velo.yamigiku on 2019/10/03.
+//  Copyright © 2019 velo.yamigiku. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var todoList = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        
+        cell.textLabel?.text = todoList[indexPath.row]
+        
+        return cell
+        
+    }
+
+}
